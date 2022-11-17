c2f0ca60b2d76d7852cd47f40e558cc68809ca94
Faith Vaz <faith.m.vaz@gmail.com>
2022-11-16 19:44:55 -0700

    added express router

---
+++ routes/apiRoutes.js
@@ -0,0 +1,53 @@
+const route = require('express').Router();
+
+const fsUtils = require('../helpers/fsUtils');
+const readAndAppend = fsUtils.readAndAppend;
+const readAndDelete = fsUtils.readAndDelete;
+const readFromFile = fsUtils.readFromFile;
+
+const { v4: uuidv4 } = require('uuid');
+const uuid = uuidv4();
+
+// Get request for notes
+route.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// Post request for submitting note
+route.post('/notes', (req, res) => {
+
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+    console.log(req.body);
+
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            id: uuid
+        }
+        // Read db.json file and push new note into data
+        readAndAppend(newNote, 'db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+
+    } else {
+        res.status(500).json('Error in posting note!');
+    }
+});
+
+route.delete('/notes/:id', (req, res) => {
+    const deletedId = req.params.id
+    readAndDelete(deletedId, 'db/db.json')
+    
+});
+
+module.exports = route;
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const notesData = require('./db/db.json');
-const { readAndAppend, readAndDelete } = require('./helpers/fsUtils');
-const fs = require('fs');
+
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-const uuid = uuidv4();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
 
 // GET Route for homepage
 app.get('/', (req, res) =>
@@ -24,51 +22,5 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-
-app.route('/api/notes')
-    // Get request for notes
-    .get((req, res) => res.json(notesData))
-
-
-
-    // Post request for submitting note
-    .post((req, res) => {
-
-        // Destructuring assignment for the items in req.body
-        const { title, text } = req.body;
-        console.log(req.body);
-
-        // If all the required properties are present
-        if (title && text) {
-            // Variable for the object we will save
-            const newNote = {
-                title,
-                text,
-                id: uuid
-            }
-            // Read db.json file and push new note into data
-            readAndAppend(newNote, './db/db.json')
-                .then(res.json(notesData));
-
-            const response = {
-                status: 'success',
-                body: newNote,
-            };
-
-            console.log(response);
-            res.status(201).json(response);
-
-        } else {
-            res.status(500).json('Error in posting note!');
-        }
-    });
-
-app.delete('/api/notes/:id', (req, res) => {
-    const deletedId = req.params.id
-    readAndDelete(deletedId, './db/db.json')
-        .then(res.json(notesData));
-
-});
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
