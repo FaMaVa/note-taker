42255b9b48bfee3de5eb578ae5a5b73291d637f6
Faith Vaz <faith.m.vaz@gmail.com>
2022-11-16 18:47:04 -0700

    bug fix

--- server.js
+++ server.js
@@ -27,46 +27,48 @@ app.get('/notes', (req, res) =>
 
 
 app.route('/api/notes')
-// Get request for notes
+    // Get request for notes
     .get((req, res) => res.json(notesData))
 
 
 
-// Post request for submitting note
+    // Post request for submitting note
     .post((req, res) => {
 
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-    console.log(req.body);
+        // Destructuring assignment for the items in req.body
+        const { title, text } = req.body;
+        console.log(req.body);
 
-    // If all the required properties are present
-    if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-            id: uuid
+        // If all the required properties are present
+        if (title && text) {
+            // Variable for the object we will save
+            const newNote = {
+                title,
+                text,
+                id: uuid
+            }
+            // Read db.json file and push new note into data
+            readAndAppend(newNote, './db/db.json')
+                .then(res.json(notesData));
+
+            const response = {
+                status: 'success',
+                body: newNote,
+            };
+
+            console.log(response);
+            res.status(201).json(response);
+
+        } else {
+            res.status(500).json('Error in posting note!');
         }
-        // Read db.json file and push new note into data
-        readAndAppend(newNote, './db/db.json');
-
-        const response = {
-            status: 'success',
-            body: newNote,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
-
-    } else {
-        res.status(500).json('Error in posting note!');
-    }
-});
+    });
 
 app.delete('/api/notes/:id', (req, res) => {
     const deletedId = req.params.id
     readAndDelete(deletedId, './db/db.json')
-    
+        .then(res.json(notesData));
+
 });
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
